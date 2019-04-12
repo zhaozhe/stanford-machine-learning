64a1936743359d1f073063e3c8ab5254d7e307b8
Zhe Z <zhaozhe100@gmail.com>
2019-04-12 12:21:13 +0200

    ex6
    week 7 finished!

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -24,10 +24,23 @@ sigma = 0.3;
 %
 
 
+Cs = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigmas = [0.01 0.03 0.1 0.3 1 3 10 30];
 
+for i = 1: length(Cs)
+	for j = 1: length(sigmas)
+		C = Cs(i);
+		sigma = sigmas(j);
+		model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+		predictions = svmPredict(model, Xval);
+		predictions_error(i, j) = mean(double(predictions~=yval));
+	end
+end
 
-
-
+mini = min(min(predictions_error));
+[i, j] = find(predictions_error == mini);
+C = Cs(i);
+sigma = sigmas(j);
 
 % =========================================================================
 
--- machine-learning-ex6/ex6/processEmail.m
+++ machine-learning-ex6/ex6/processEmail.m
@@ -96,7 +96,11 @@ while ~isempty(email_contents)
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
     %
-
+    for w=1:length(vocabList)
+        if strcmp(vocabList{w}, str)
+            word_indices = [word_indices ; w];
+        end
+    end
 
 
 
